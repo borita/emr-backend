b5eddfc32dbebf3a088a7f86e4448664bfe26642
César Guerra <warderer@gmail.com>
2020-05-17 16:13:05 -0500

    Fixed Encounters Patch Service with lodash.merge

--- services/EncountersService.js
+++ services/EncountersService.js
@@ -1,21 +1,12 @@
-require('nested-object-assign');
 const Encounters = require('../models/Encounters');
-console.log("llego a services")
+const merge = require('lodash.merge');
+
 module.exports = {
   create: (body) => Encounters.create(body),
   find: () => Encounters.find(),
   findById: (id) => Encounters.findById(id),
-
-  //Superheroe.findOneAndUpdate({_id: idHeroe},{$set:keysToUpdate},{new:true}).exec()
   update: (encounter, body) => {
-    //encounter = JSON.parse(JSON.stringify(body));
-    //let dr  = subjective.rendering; 
-    //Object.assign(encounter,body);
-    
-    console.log("--------------------")
-    nestedObjectAssign(encounter,JSON.parse(JSON.stringify(body)));
-    console.log(nestedObjectAssign(encounter,JSON.parse(JSON.stringify(body))))
-    return encounter.save();
-  },
+    const merged = merge(encounter,body);
+    return merged.save();
+  }
 }
-
