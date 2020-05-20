0ec34de1cdd6308b0c9c3a60f45f6a2b90a141ab
César Guerra <warderer@gmail.com>
2020-05-19 19:00:26 -0500

    Add bcrypt and save encrypted password

--- models/Users.js
+++ models/Users.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
 
 const usersSchema = new Schema({
     username: {
@@ -20,6 +22,27 @@ const usersSchema = new Schema({
         type: Boolean,
         default: true,
     },
+});
+
+usersSchema.pre('save', function (next) {
+    const user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
 });
 
 const Users = mongoose.model('User', usersSchema);
