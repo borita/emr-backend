d46d051133f7540521a7b58dcc69c80d759dd1ee
César Guerra <warderer@gmail.com>
2020-05-20 20:36:53 -0500

    Add CORS Support

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const server = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 console.log(PORT)
+
+//CORS
+server.use(cors());
+
 // Endpoints
 server.get('/', (req, res) => res.send('Hello World!'));
 
